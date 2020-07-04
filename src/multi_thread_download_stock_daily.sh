3f37df27e87dfaadf249550381824b81736b6961
luyan <beahcer.lu@qq.com>
2020-07-04 20:56:36 +0800

    add gitignore

--- src/multi_thread_download_stock_daily.sh
+++ src/multi_thread_download_stock_daily.sh
@@ -1,8 +1,9 @@
-python	download_stock_daily_data_once.py	5	0       20200619 &
-python	download_stock_daily_data_once.py	5	1	20200619 &
-python	download_stock_daily_data_once.py	5	2	20200619 &
-python	download_stock_daily_data_once.py	5	3	20200619 &
-python	download_stock_daily_data_once.py	5	4	20200619 &
+
+python	download_stock_daily_data_once.py	5	0       20200624 &
+python	download_stock_daily_data_once.py	5	1	20200624 &
+python	download_stock_daily_data_once.py	5	2	20200624 &
+python	download_stock_daily_data_once.py	5	3	20200624 &
+python	download_stock_daily_data_once.py	5	4	20200624 &
 
 python	download_stock_daily_data_once.py	5	0       20200622 &
 python	download_stock_daily_data_once.py	5	1	20200622 &
@@ -10,14 +11,9 @@ python	download_stock_daily_data_once.py	5	2	20200622 &
 python	download_stock_daily_data_once.py	5	3	20200622 &
 python	download_stock_daily_data_once.py	5	4	20200622 &
 
-python	download_stock_daily_data_once.py	5	0       20200623 &
-python	download_stock_daily_data_once.py	5	1	20200623 &
-python	download_stock_daily_data_once.py	5	2	20200623 &
-python	download_stock_daily_data_once.py	5	3	20200623 &
-python	download_stock_daily_data_once.py	5	4	20200623 &
+python	download_stock_daily_data_once.py	5	0       20200619 &
+python	download_stock_daily_data_once.py	5	1	20200619 &
+python	download_stock_daily_data_once.py	5	2	20200619 &
+python	download_stock_daily_data_once.py	5	3	20200619 &
+python	download_stock_daily_data_once.py	5	4	20200619 &
 
-python	download_stock_daily_data_once.py	5	0       20200624 &
-python	download_stock_daily_data_once.py	5	1	20200624 &
-python	download_stock_daily_data_once.py	5	2	20200624 &
-python	download_stock_daily_data_once.py	5	3	20200624 &
-python	download_stock_daily_data_once.py	5	4	20200624 &
