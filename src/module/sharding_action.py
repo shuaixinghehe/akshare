13d4745bb54cfef4c57bcef7480743100f548455
luyan <beahcer.lu@qq.com>
2020-07-05 01:28:41 +0800

    add

--- src/module/sharding_action.py
+++ src/module/sharding_action.py
@@ -1,13 +1,14 @@
 # coding:utf8
 
 import pymysql.cursors
+import time
 
-ts_conn = pymysql.connect(host="127.0.0.1", user="tushare", \
-                          password="&QwX0^4#Sm^&t%V6wBnZC%78", \
-                          db="tushare", \
-                          charset='utf8')
-
-ts_cur = ts_conn.cursor()
+# ts_conn = pymysql.connect(host="127.0.0.1", user="tushare", \
+#                           password="&QwX0^4#Sm^&t%V6wBnZC%78", \
+#                           db="tushare", \
+#                           charset='utf8')
+#
+# ts_cur = ts_conn.cursor()
 
 ak_conn = pymysql.connect(host="127.0.0.1", user="tushare", \
                           password="&QwX0^4#Sm^&t%V6wBnZC%78", \
@@ -33,18 +34,53 @@ def get_ts_code_list():
     return ts_code_list
 
 
-def sharding_action_table():
+insert_ts_code_map = {}
+
+
+def sharding_action_table(batch_size=0, offset_pos=0):
     pass
-    sql = """
-            select * from stock_realtime_action limit 10
-        """
+    sql = """ select * from stock_realtime_action limit """ + str(offset_pos) + "," + str(batch_size)
+    print("sql", sql)
     ak_cur.execute(sql, ())
     result = ak_cur.fetchall()
+    insert_sql = ""
+
     for row in result:
-        print(row[0])
-        # insert_sql = """
-        #     insert into table stock_realtime_action_
-        # """
+        table_name_part = str(row[0]).replace(".", "")
+        insert_sql = """  ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}'),""".format(
+            row[0],
+            row[1],
+            row[2],
+            row[3],
+            row[4],
+            row[5],
+            row[6],
+            row[7],
+        )
+        if table_name_part in insert_ts_code_map.keys():
+            pass
+            insert_ts_code_map[table_name_part] += insert_sql
+        else:
+            pass
+            insert_ts_code_map[table_name_part] = insert_sql
+    exce_insert_ts_code_map(insert_ts_code_map, 10)
+
+
+def exce_insert_ts_code_map(map={}, limit=1000):
+    pass
+    insert_sql = ""
+    for key in map.keys():
+        if len(insert_ts_code_map[key]) > limit:
+            insert_sql = """ insert into stock_realtime_action_""" + key + " (ts_code,trade_date,trade_time,price,price_change,volumn,value,kind) values "
+            insert_sql += insert_ts_code_map[key][0:-1]
+            insert_sql += ";"
+            # print("merge sql", insert_sql)
+            insert_ts_code_map[key] = ""
+            ak_cur.execute(insert_sql, ())
+            ak_conn.commit()
+    # print("insert_sql->", insert_sql)
+    # ak_cur.execute(insert_sql, ())
+    # ak_conn.commit()
 
 
 def create_shareding_table(ts_code='default'):
@@ -69,7 +105,48 @@ def create_shareding_table(ts_code='default'):
         ak_conn.commit()
 
 
+def truncate_shareding_table(ts_code='default'):
+    pass
+    ts_code_list = get_ts_code_list()
+    sql = ""
+    for code in ts_code_list:
+        print(code, str(code).replace('.', ""))
+        ts_code = str(code).replace('.', "")
+        sql = "select * from  stock_realtime_action_" + ts_code
+        ak_cur.execute(sql, ())
+        result = ak_cur.fetchall()
+        if len(result) > 0:
+            sql = """ truncate table stock_realtime_action_""" + ts_code + """;"""
+            print(sql)
+            ak_cur.execute(sql, ())
+            ak_conn.commit()
+            time.sleep(1)
+        else:
+            print(ts_code, " is empty", len(result))
+
+
+def start_sharding():
+    pass
+    batch_size = 500
+    end_round = 30000
+    for i in range(100, end_round):
+        start_time = time.time()
+        sharding_action_table(batch_size, offset_pos=i * batch_size)
+        print(i, " round  cost ", time.time() - start_time)
+    print(insert_ts_code_map)
+    exce_insert_ts_code_map(insert_ts_code_map, 0)
+
+
 if __name__ == '__main__':
     pass
-    create_shareding_table()
-    # sharding_action_table()
+    # create_shareding_table()
+    # sharding_action_table(batch_size=10000, offset_pos=0)
+    # start_sharding()
+    truncate_shareding_table()
+    # sql = """
+    #        (ts_code,trade_date,trade_time,price,price_change,volumn,value,kind)
+    #         values
+    #         ({0},%s,%s,%s,  %s,%s,%s,%s );
+    #     """
+    # sql = sql.format(1, 2, 3, 4, 5, 6, 7, 8)
+    # print(sql)
