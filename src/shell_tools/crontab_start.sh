ee166bbca3c72abe8d0cb698e51021dbcb52cb47
luyan <beahcer.lu@qq.com>
2020-11-21 23:50:51 +0800

    add

--- src/shell_tools/crontab_start.sh
+++ src/shell_tools/crontab_start.sh
@@ -4,14 +4,14 @@ v_time=`date "+%Y_%m_%d"`
 run_date=`date "+%Y%m%d"`
 cd /Users/beacher/Workspace/custom_virtual_env_space
 source ./akshare_env/bin/activate
-cd /Users/beacher/Workspace/akshare/src/module
+cd /Users/beacher/Workspace/akshare/src/download_data_module
 mode_value=1
 for((i=0;i<$mode_value;i++))
 do
     python download_stock_daily_data_once.py $mode_value $i $run_date >> /tmp/download_stock_daily_data_once_mode_"$i"_$v_time.log &
     python download_realtime_action_data_once.py $mode_value $i $run_date >> /tmp/download_realtime_action_data_once_mode_"$i"_$v_time.log &
 done
-cd module
+cd /Users/beacher/Workspace/akshare/src/module
 python stock_daily_report_action.py $run_date >> /tmp/stock_daily_report_action_$v_time.log
 
 
